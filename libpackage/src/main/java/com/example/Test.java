862f60d1312942734c32c4baeef32970fe0239da
home-coder <one_face@sina.com>
2017-06-08 22:40:54 +0800

    what how why, package

---
+++ libpackage/src/main/java/com/example/Test.java
@@ -0,0 +1,8 @@
+package com.example;
+
+/* example, hakuna --->what, how, why */
+public class Test {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ libpackage/src/main/java/com/hakuna/Test.java
@@ -0,0 +1,6 @@
+package com.hakuna;
+/**
+ * Created by java on 17-6-8.
+ */
+public class Test {
+}
